e4c81a0c15d2fc2809a517882bb2b5bd25e88640
Gabriel Campero Durand <campero@ovgu.de>
2019-11-20 10:27:06 +0100

    Checking about some randomness for the task, still not in TF

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ FLAGS = tf.flags.FLAGS
 
 # Model parameters
 tf.flags.DEFINE_integer("hidden_size", 64, "Size of LSTM hidden layer.")
-tf.flags.DEFINE_integer("memory_size", 16, "The number of memory slots.")
-tf.flags.DEFINE_integer("word_size", 16, "The width of each memory slot.")
+tf.flags.DEFINE_integer("memory_size", 20, "The number of memory slots.")
+tf.flags.DEFINE_integer("word_size", 20, "The width of each memory slot.")
 tf.flags.DEFINE_integer("num_write_heads", 1, "Number of memory write heads.")
 tf.flags.DEFINE_integer("num_read_heads", 4, "Number of memory read heads.")
 tf.flags.DEFINE_integer("clip_value", 20,
@@ -42,7 +42,7 @@ tf.flags.DEFINE_float("optimizer_epsilon", 1e-10,
                       "Epsilon used for RMSProp optimizer.")
 
 # Task parameters
-tf.flags.DEFINE_integer("batch_size", 16, "Batch size for training.")
+tf.flags.DEFINE_integer("batch_size", 24, "Batch size for training.")
 tf.flags.DEFINE_integer("num_bits", 8, "Dimensionality of each vector to copy")
 tf.flags.DEFINE_integer(
     "min_length", 1,
@@ -99,6 +99,10 @@ def train(num_training_iterations, report_interval):
                                    FLAGS.min_repeats, FLAGS.max_repeats)
   dataset_tensors = dataset()
 
+  #print("TO")
+  #print(dataset_tensors.observations)
+  #print("TS")
+  #print(dataset.target_size)
   output_logits = run_model(dataset_tensors.observations, dataset.target_size)
   # Used for visualization.
   output = tf.round(
